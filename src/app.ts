55d9ea9d377b81069c430556756d1b886a7ab165
Ezaz <ezaz.dev@gmail.com>
2022-10-15 23:37:43 +0600

    postgresql added

--- src/app.ts
+++ src/app.ts
@@ -1,45 +1,48 @@
-import Fastify, { FastifyRequest, FastifyReply } from "fastify";
+import Fastify, { FastifyRequest, FastifyReply } from 'fastify'
 import fjwt from '@fastify/jwt'
 
-import userRoutes from "./modules/user/user.route";
-import { userSchemas } from "./modules/user/user.schema";
+import userRoutes from './modules/user/user.route'
+import { userSchemas } from './modules/user/user.schema'
 
 export const fastify = Fastify()
 
-declare module "fastify" {
+declare module 'fastify' {
   export interface FastifyInstance {
     authenticate: 'any'
   }
 }
 
 fastify.register(fjwt, {
-  secret: "supersecret"
+  secret: 'supersecret',
 })
 
-fastify.decorate("authenticate", async (request: FastifyRequest, reply: FastifyReply) => {
-  try {
-    await request.jwtVerify()
-  } catch (error) {
-    return reply.send(error)
+fastify.decorate(
+  'authenticate',
+  async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+      await request.jwtVerify()
+    } catch (error) {
+      return reply.send(error)
+    }
   }
-})
+)
 
 fastify.get('/healthcheck', async () => {
-  return { status: "OK" }
+  return { status: 'OK' }
 })
 
 async function main() {
-
   for (const schema of userSchemas) {
     fastify.addSchema(schema)
   }
 
   fastify.register(userRoutes, {
-    prefix: 'api/users'
+    prefix: 'api/users',
   })
 
-  fastify.listen({ port: 3000, host: '0.0.0.0' })
-    .then((address) => console.log(`server listening on ${address}`))
+  fastify
+    .listen({ port: 3000, host: '0.0.0.0' })
+    .then(address => console.log(`server listening on ${address}`))
     .catch(err => {
       console.log('Error starting server:', err)
       process.exit(1)
