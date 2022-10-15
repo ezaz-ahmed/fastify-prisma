2098e614f1f20892bc02f3c7cc34a27a688e698f
Ezaz <ezaz.dev@gmail.com>
2022-10-16 01:03:38 +0600

    bare bone done & structure ready

--- src/app.ts
+++ src/app.ts
@@ -1,52 +1,19 @@
-import Fastify, { FastifyRequest, FastifyReply } from 'fastify'
-import fjwt from '@fastify/jwt'
+import fastify from 'fastify'
 
-import userRoutes from './modules/user/user.route'
-import { userSchemas } from './modules/user/user.schema'
+const app = fastify()
 
-export const fastify = Fastify()
-
-declare module 'fastify' {
-  export interface FastifyInstance {
-    authenticate: 'any'
-  }
-}
-
-fastify.register(fjwt, {
-  secret: 'supersecret',
-})
-
-fastify.decorate(
-  'authenticate',
-  async (request: FastifyRequest, reply: FastifyReply) => {
-    try {
-      await request.jwtVerify()
-    } catch (error) {
-      return reply.send(error)
-    }
-  }
-)
-
-fastify.get('/healthcheck', async () => {
+app.get('/healthcheck', async () => {
   return { status: 'OK' }
 })
 
 async function main() {
-  for (const schema of userSchemas) {
-    fastify.addSchema(schema)
+  try {
+    await app.listen({ port: 3000, host: '0.0.0.0' })
+    console.log('Server is ready at port 3000')
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
   }
-
-  fastify.register(userRoutes, {
-    prefix: 'api/users',
-  })
-
-  fastify
-    .listen({ port: 3000, host: '0.0.0.0' })
-    .then(address => console.log(`server listening on ${address}`))
-    .catch(err => {
-      console.log('Error starting server:', err)
-      process.exit(1)
-    })
 }
 
 main()
