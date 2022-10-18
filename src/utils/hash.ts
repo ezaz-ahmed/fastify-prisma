983812c665087fea110c3b12b246f6b1594938f8
Ezaz <ezaz.dev@gmail.com>
2022-10-19 00:55:24 +0600

    💚 Custom hash password added

---
+++ src/utils/hash.ts
@@ -0,0 +1,27 @@
+import { pbkdf2Sync, randomBytes } from 'crypto'
+
+export function hashPassword(password: string) {
+  const salt = randomBytes(16).toString('hex')
+  const hash = pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex')
+  return { hash, salt }
+}
+
+export function verifiedPassword({
+  providedPass,
+  salt,
+  hash,
+}: {
+  providedPass: string
+  salt: string
+  hash: string
+}) {
+  const candidateHash = pbkdf2Sync(
+    providedPass,
+    salt,
+    1000,
+    64,
+    'sha512'
+  ).toString('hex')
+
+  return candidateHash === hash
+}
