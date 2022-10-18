f3ac888a9701a053ce51db2bec3574ff99a74fdb
Ezaz <ezaz.dev@gmail.com>
2022-10-18 23:38:39 +0600

    ➕â Prisma migration added

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,5 +1,8 @@
 import { FastifyInstance } from 'fastify'
+import { registerUserHandler } from './user.controller'
 
-async function userRoutes(server: FastifyInstance) {}
+async function userRoutes(server: FastifyInstance) {
+  server.post('/', registerUserHandler)
+}
 
 export default userRoutes
